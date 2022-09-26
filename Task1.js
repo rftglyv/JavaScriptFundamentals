7f4a18f999893ba1d10ce06c1a23df429d4cc852
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-09-26 21:08:50 +0400

    Task Completed

--- Task1.js
+++ Task1.js
@@ -1,9 +1,9 @@
-let userName = "ElektrikləşdirdikLərdinizdənsinizMi"
+let userName = "ElektrikləŞdirdikLƏrdinizdənsinizmi"
 
 const main = str => {
     let vowels = /[aıoueəiöü]/gi;
     let result = str.match(vowels);
-    let upper = str.match(/[A-Z]/g);
+    let upper = str.match(/[A-Z,Ü,Ö,Ğ,Ə,Ç,Ş]/g);
     console.log("Sait hərflərin sayı : " + result.length + "\n" + "Sözdəki hərf sayı : " 
     + str.length + "\n" + "Söz daxilində olan böyük hərf sayı : " + upper.length + " " + "(" + upper + ")");
 };
