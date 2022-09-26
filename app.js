f786aff0e645387b2ce35f0ce57ac8d6b0ce9e14
Rufat Guliyev <47758460+rftglyv@users.noreply.github.com>
2022-09-26 19:50:39 +0400

    Task Completed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const main = str => {
     let result = str.match(vowels);
     let count = result.length;
     let upper = str.match(/[A-Z]/g);
-    console.log("Sait hərflərin sayı : " + count + "\n" + "Söz daxilində olan böyük hərf sayı : " + upper.length + "(" + upper + ")");
+    console.log("Sait hərflərin sayı : " + count + "\n" + "Sözdəki hərf sayı : " + str.length + "\n" + "Söz daxilində olan böyük hərf sayı : " + upper.length + "(" + upper + ")");
 };
 
 main(userName)
